0572631b9ab424ef8b2265d22fbf0d75554247fd
Thales Yuji <thalesyuuji@gmail.com>
2025-10-08 19:48:36 -0300

    frontend stripe

--- marketplace-freelancers-frontend/src/Servicos/Api.js
+++ marketplace-freelancers-frontend/src/Servicos/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://web-production-385bb.up.railway.app/api",
 });
 
 api.interceptors.request.use(
