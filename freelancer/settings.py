d300eab088b57f0c85671bef69350e1ab585991d
Thales Yuji <thalesyuuji@gmail.com>
2025-10-08 20:05:26 -0300

    Fix CORS headers

--- freelancer/settings.py
+++ freelancer/settings.py
@@ -186,6 +186,19 @@ CORS_ALLOWED_ORIGINS = os.getenv(
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # ------------------------
 # CSRF
 # ------------------------
