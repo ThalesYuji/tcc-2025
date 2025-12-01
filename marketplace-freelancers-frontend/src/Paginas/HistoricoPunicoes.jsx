dfc52e6954868110fa4448568bb19abcdcb98815
Thales Yuji <thalesyuuji@gmail.com>
2025-12-01 03:42:41 -0300

    denuncias admin ajuste

--- marketplace-freelancers-frontend/src/Paginas/HistoricoPunicoes.jsx
+++ marketplace-freelancers-frontend/src/Paginas/HistoricoPunicoes.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { listarHistoricoPunicoes, removerPunicao } from "../Servicos/Api";
 import {
   FaGavel,
-  FaSearch,
   FaTimes,
   FaRedoAlt,
   FaExclamationCircle,
